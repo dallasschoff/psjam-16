1f88c61ad7372a1678402dd0c9b50358dfb6fa4b
Krieglliam <billyjr129@gmail.com>
2025-01-25 03:02:33 -0500

    Fixed "enter" bug from main menu

--- pirate-software-jam-16/Scripts/main_menu.gd
+++ pirate-software-jam-16/Scripts/main_menu.gd
@@ -2,10 +2,8 @@ extends Control
 
 var game_started = false
 
-func _process(float) -> void:
-	if Input.is_action_just_pressed("enter") and not game_started:
-		_on_start_button_pressed()
-		game_started = true
+func _ready():
+	$"MarginContainer/VBoxContainer/Start Button".grab_focus()
 
 func _on_start_button_pressed():
 	SignalBus.start_game.emit()
