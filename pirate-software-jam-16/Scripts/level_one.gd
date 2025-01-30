a5cdf511ba8347948e9c5451ab6bd5701b4e579c
Krieglliam <billyjr129@gmail.com>
2025-01-29 23:48:32 -0500

    Added weapon UI

--- pirate-software-jam-16/Scripts/level_one.gd
+++ pirate-software-jam-16/Scripts/level_one.gd
@@ -4,6 +4,10 @@ var orcs: Array[Node]
 var originalOrcCount: int
 var objective_complete: bool
 var handled_win: bool
+@onready var player = $Player
+@onready var ui_bg = $Camera2D/bg
+@onready var weapon_ui = $Camera2D/WeaponUI
+var weapon_ui_active: bool = false
 
 func _ready():
 	orcs = get_tree().get_nodes_in_group("Orcs")
@@ -11,8 +15,26 @@ func _ready():
 	$TileMapLayer.visible = false
 
 func _process(delta):
+	#Handle win
 	orcs = get_tree().get_nodes_in_group("Orcs")
 	objective_complete = orcs.size() < originalOrcCount
 	if objective_complete and not handled_win:
 		handled_win = true
 		Global.master._next_level()
+	#Handle weapon UI
+	if player.isPossessing and not weapon_ui_active:
+		weapon_ui_active = true
+		var tween = get_tree().create_tween()
+		tween.tween_property(ui_bg, "position", Vector2(0, 101), 0.8)
+		tween.tween_property(ui_bg, "position", Vector2(0, 103), 0.2)
+		var tween2 = get_tree().create_tween()
+		tween2.tween_property(weapon_ui, "position", Vector2(0, 101), 0.8)
+		tween2.tween_property(weapon_ui, "position", Vector2(0, 103), 0.2)
+	if player.isPossessing == false and weapon_ui_active:
+		weapon_ui_active = false
+		var tween = get_tree().create_tween()
+		tween.tween_property(ui_bg, "position", Vector2(0, 101), 0.2)
+		tween.tween_property(ui_bg, "position", Vector2(0, 139), 0.8)
+		var tween2 = get_tree().create_tween()
+		tween2.tween_property(weapon_ui, "position", Vector2(0, 101), 0.2)
+		tween2.tween_property(weapon_ui, "position", Vector2(0, 139), 0.8)
--- pirate-software-jam-16/Scripts/level_two.gd
+++ pirate-software-jam-16/Scripts/level_two.gd
@@ -6,6 +6,10 @@ var targets: Array[Node]
 var objective_complete: bool
 var handled_win: bool
 var level_ended: bool = false
+@onready var player = $Player
+@onready var ui_bg = $Camera2D/bg
+@onready var weapon_ui = $Camera2D/WeaponUI
+var weapon_ui_active: bool = false
 
 func _ready():
 	orcs = get_tree().get_nodes_in_group("Orcs")
@@ -30,3 +34,20 @@ func _process(delta):
 		#await get_tree().create_timer(2).timeout
 		#get_tree().reload_current_scene()
 		Global.master._next_level()
+	#Handle weapon UI
+	if player.isPossessing and not weapon_ui_active:
+		weapon_ui_active = true
+		var tween = get_tree().create_tween()
+		tween.tween_property(ui_bg, "position", Vector2(0, 101), 0.8)
+		tween.tween_property(ui_bg, "position", Vector2(0, 103), 0.2)
+		var tween2 = get_tree().create_tween()
+		tween2.tween_property(weapon_ui, "position", Vector2(0, 101), 0.8)
+		tween2.tween_property(weapon_ui, "position", Vector2(0, 103), 0.2)
+	if player.isPossessing == false and weapon_ui_active:
+		weapon_ui_active = false
+		var tween = get_tree().create_tween()
+		tween.tween_property(ui_bg, "position", Vector2(0, 101), 0.2)
+		tween.tween_property(ui_bg, "position", Vector2(0, 139), 0.8)
+		var tween2 = get_tree().create_tween()
+		tween2.tween_property(weapon_ui, "position", Vector2(0, 101), 0.2)
+		tween2.tween_property(weapon_ui, "position", Vector2(0, 139), 0.8)
