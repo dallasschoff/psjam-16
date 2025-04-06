57e6fac8921698f29fe83c7cdd4332f112ffbc59
Krieglliam <billyjr129@gmail.com>
2025-04-06 01:22:16 -0400

    Changed project settings so "Emulate mouse from touch was = true"
    The joystick has an error print if this is set, but another source says it's required for buttons

--- pirate-software-jam-16/Scripts/main_menu.gd
+++ pirate-software-jam-16/Scripts/main_menu.gd
@@ -11,10 +11,22 @@ func _ready():
 func _on_start_button_pressed():
 	$"MarginContainer/VBoxContainer/Start Button".release_focus()
 	Global.Transitioner._fade(true)
-	
+
+func _on_touch_screen_button_pressed() -> void:
+	$"MarginContainer/VBoxContainer/Start Button".release_focus()
+	Global.Transitioner._fade(true)
+
 func _start_game():
 	SignalBus.start_game.emit() #Connects to master
 
 func _on_mobile_controls_button_toggled(toggled_on: bool) -> void:
 	if toggled_on == true: mobileControls = true
 	else: mobileControls = false
+
+
+func _on_touch_screen_button_1_pressed() -> void:
+	if $MobileControlsButton.button_pressed == false: 
+		$MobileControlsButton.button_pressed = true
+		mobileControls = true
+	else: mobileControls = false
+	#if $MobileControlsButton.button_pressed = true
